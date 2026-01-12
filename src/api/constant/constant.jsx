9cf9deff7f32cfacd027a2dc4c995a1c510d51a0
Rakiu <sainiravindra369@gmail.com>
2026-01-12 14:31:24 +0530

    add

--- src/api/constant/constant.jsx
+++ src/api/constant/constant.jsx
@@ -12,3 +12,6 @@ export const GET_ALL_LOANS_ADMIN = "/api/loans/admin/loans";
 export const APPROVE_LOAN = (id) => `/api/loans/admin/loan/${id}/approve`;
 export const REJECT_LOAN = (id) => `/api/loans/admin/loan/${id}/restart`;
 export const GET_LOAN_DETAILS = (id) => `/api/loans/admin/loan/${id}`;
+
+
+
